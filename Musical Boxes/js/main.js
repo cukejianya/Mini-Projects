9f739f020db074a2f0c0a703810fec2e48a55dd6
Chinedum Mac <cukejianya@gmail.com>
2015-02-17 19:25:17 -0500

    add http server and able to form wavesurfer; however, produces two waves instead of one

--- Musical Boxes/js/main.js
+++ Musical Boxes/js/main.js
@@ -1,19 +1,52 @@
-var song;
+var wavesurfer;
 $('div').mousedown(function(){
 	var id = $(event.target).attr('id');
-	switch(id){
-		case 'c':
-			elm = 'first';
-			break;
 
-		default:
-			elm = 0;
-			break;
-	}
-		song = document.getElementById(elm);
-		song.currentTime = 0;
-		song.play();
+//		song = document.getElementById(elm);
+//		url = (d)
+//		song.currentTime = 0;
+//		song.play();
+		wavesurfer = Object.create(WaveSurfer);
+
+		wavesurfer.init({
+			container: document.querySelector('#wave'),
+			waveColor: 'violet',
+			progressColor: 'purple'
+		});
+
+		wavesurfer.on('ready', function () {
+			wavesurfer.play();
+		});
+		
+		
+		switch(id){
+			case 'c':
+				wavesurfer.load('../music/TidusTheme.ogg');
+				break;
+			case 'd':
+				elm = 'second';
+				break;
+			case 'e':
+				elm = 'third';
+				break;
+			case 'f':
+				elm = 'fourth';
+				break;
+			case 'g':
+				elm = 'fifth';
+				break;
+			case 'a':
+				elm = 'sixth';
+				break;
+			case 'b':
+				elm = 'seventh';
+				break;
+			default:
+				elm = 0;
+				break;
+		}
 });
-$(document).mouseup(function(){
-	song.pause();
-})
+
+$('div').mouseup(function(){
+	wavesurfer.destory();
+})	
