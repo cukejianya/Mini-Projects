ea8dce6df6e17acf18eae2950817e14a11afb722
Chinedum Mac <cukejianya@gmail.com>
2015-02-17 23:15:33 -0500

    Solve the event running through twice with e.stopPropagation also add the other songs to main.js. USe color of different div base on id for progressColor. Also able to elimante div and wave after mouseup

--- Musical Boxes/js/main.js
+++ Musical Boxes/js/main.js
@@ -1,52 +1,92 @@
-var wavesurfer;
-$('div').mousedown(function(){
-	var id = $(event.target).attr('id');
+'use strict';
 
+var wavesurfer;
+var x =0;
+
+$('div').mousedown(function(e){
+	//Prevents event to run more than once per click
+	e.stopPropagation();
+
+	var id = '#'+$(event.target).attr('id');
 //		song = document.getElementById(elm);
 //		url = (d)
 //		song.currentTime = 0;
 //		song.play();
+	var pColor = $(id+':active').css('background-color');
+	
+	var options = {
+        container		: document.querySelector('#wave'),
+        waveColor		: '#333333',
+		progressColor	: pColor,
+        loaderColor   	: 'purple',
+        cursorColor   	: 'navy'
+        // scrollParent	: true,
+        // minPxPerSex		: 200
+    };
+    x++;
+    console.log(x);
+
 		wavesurfer = Object.create(WaveSurfer);
-
-		wavesurfer.init({
-			container: document.querySelector('#wave'),
-			waveColor: 'violet',
-			progressColor: 'purple'
-		});
-
-		wavesurfer.on('ready', function () {
-			wavesurfer.play();
-		});
-		
-		
+		wavesurfer.init(options);
+	
+	var song;
 		switch(id){
-			case 'c':
-				wavesurfer.load('../music/TidusTheme.ogg');
+			case '#c':
+				song = 'KHTheme.ogg';
 				break;
-			case 'd':
-				elm = 'second';
+			case '#d':
+				song = 'TidusTheme.ogg';
 				break;
-			case 'e':
-				elm = 'third';
+			case '#e':
+				song = 'Final_Fantasy.ogg';
 				break;
-			case 'f':
-				elm = 'fourth';
+			case '#f':
+				song = 'EndingFF10.ogg';
 				break;
-			case 'g':
-				elm = 'fifth';
+			case '#g':
+				song = 'The_Prelude.ogg';
 				break;
-			case 'a':
-				elm = 'sixth';
+			case '#a':
+				song = 'KHDearlyBeloved.ogg';
 				break;
-			case 'b':
-				elm = 'seventh';
+			case '#b':
+				song = 'TinaFF7PianoCollections.ogg';
 				break;
 			default:
 				elm = 0;
 				break;
 		}
+	wavesurfer.load('../music/'+song);
+
+	wavesurfer.on('ready', function () {
+			wavesurfer.play();
+	});
 });
 
-$('div').mouseup(function(){
-	wavesurfer.destory();
-})	
+$(document).mouseup(function(){
+	$('#wave').empty();
+	wavesurfer.pause();
+	wavesurfer = 0;
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var progressDiv = document.querySelector('#progress-bar');
+    var progressBar = progressDiv.querySelector('.progress-bar');
+
+    var showProgress = function (percent) {
+        progressDiv.style.display = 'block';
+        progressBar.style.width = percent + '%';
+    };
+
+    var hideProgress = function () {
+        progressDiv.style.display = 'none';
+    };
+
+    wavesurfer.on('loading', showProgress);
+    wavesurfer.on('ready', hideProgress);
+    wavesurfer.on('destroy', hideProgress);
+    wavesurfer.on('error', hideProgress);
+});
+
+
+
