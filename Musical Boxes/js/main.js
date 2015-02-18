9f6c618046e1416a666ca5d0f7c7f69fc32dc8d6
Chinedum Mac <cukejianya@gmail.com>
2015-02-17 23:54:50 -0500

    got the scrollParent and minPxPerSec to work. Add comments. I also made wave div the size of the window. Everything is finish

--- Musical Boxes/js/main.js
+++ Musical Boxes/js/main.js
@@ -7,28 +7,33 @@ $('div').mousedown(function(e){
 	//Prevents event to run more than once per click
 	e.stopPropagation();
 
+	//get the id of the div being targeted
 	var id = '#'+$(event.target).attr('id');
-//		song = document.getElementById(elm);
-//		url = (d)
-//		song.currentTime = 0;
-//		song.play();
-	var pColor = $(id+':active').css('background-color');
+
+	// get back-ground color of the id of this div.
+	var pColor = $(id).css('background-color');
 	
+
+	// set options for wavesurfer.init
 	var options = {
         container		: document.querySelector('#wave'),
         waveColor		: '#333333',
 		progressColor	: pColor,
         loaderColor   	: 'purple',
-        cursorColor   	: 'navy'
-        // scrollParent	: true,
-        // minPxPerSex		: 200
+        cursorColor   	: 'black',
+        scrollParent	: true,
+        minPxPerSec		: 200,
+        hideScrollbar	: true
     };
-    x++;
-    console.log(x);
+
+
+    // x++;					Used to check how many times this event was being 						called
+    // console.log(x);
 
 		wavesurfer = Object.create(WaveSurfer);
 		wavesurfer.init(options);
 	
+	// use switch to check what the id is and base of id set var song to music file
 	var song;
 		switch(id){
 			case '#c':
@@ -53,39 +58,24 @@ $('div').mousedown(function(e){
 				song = 'TinaFF7PianoCollections.ogg';
 				break;
 			default:
-				elm = 0;
+				song = 0;
 				break;
 		}
+
+	// load the music file
 	wavesurfer.load('../music/'+song);
 
+	// play music file when loaded
 	wavesurfer.on('ready', function () {
 			wavesurfer.play();
 	});
 });
 
+// kills music wavesurfer and the div containing the wavesurfer object
 $(document).mouseup(function(){
 	$('#wave').empty();
 	wavesurfer.pause();
 	wavesurfer = 0;
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var progressDiv = document.querySelector('#progress-bar');
-    var progressBar = progressDiv.querySelector('.progress-bar');
-
-    var showProgress = function (percent) {
-        progressDiv.style.display = 'block';
-        progressBar.style.width = percent + '%';
-    };
-
-    var hideProgress = function () {
-        progressDiv.style.display = 'none';
-    };
-
-    wavesurfer.on('loading', showProgress);
-    wavesurfer.on('ready', hideProgress);
-    wavesurfer.on('destroy', hideProgress);
-    wavesurfer.on('error', hideProgress);
 });
 
 
