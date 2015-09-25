67af00353656f011c174ec6167479633367353f3
Chinedum Mac <cukejianya@gmail.com>
2015-09-25 00:45:01 -0400

    Finish groupPlacement logic

--- SudokuSolver/sudokuSolver.js
+++ SudokuSolver/sudokuSolver.js
@@ -2,22 +2,38 @@ var testarray = [[4,3,5,2,6,1,3,0], [6,7,2,1,3,null],[9,0,1,4,9,0,0], [3,2,0,2,1
 
 
 //var numbersInGroup = {1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false}
+var submit = document.getElementById('submit');
+
+
 sudokuGrid = [];
 
 function getInput() {
   var arrayInput = document.getElementsByTagName('input');
   for (var i = 0; i < arrayInput.length; i++){
-    cellInfoMap = {
-      'value': parseInt(document.getElementsByTagName('input')[i].value),
-      'coord': document.getElementsByTagName('input')[i].name,
-      'group':
-    };
-    sudokuGrid[]
-  }
+    var cell = document.getElementsByTagName('input')[i];
+    var coord = cell.name.split(',').map(function(a){return parseInt(a)});
 
+    var cellInfoMap = {
+      'value': parseInt(cell.value),
+      'coord': coord,
+      'group': groupPlacemant(coord)
+    };
+
+    sudokuGrid[i] = cellInfoMap;
+  }
 }
 
+function groupPlacemant(array) {
+  var row = array[0];
+  var col = array[1];
+  row %= 3;
+  col %= 3;
 
+  var groupNumber = 3*row+(++col);
 
+  return groupNumber;
+}
 
-console.log(Group1.idx(7));
+submit.addEventListener('click', getInput);
+
+console.log(sudokuGrid);
