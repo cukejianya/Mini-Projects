9c303feb9b5b52dc0a9001adfe3d8e3a76f447ef
Chinedum Mac <cukejianya@gmail.com>
2015-09-25 21:28:23 -0400

    Update solver logic

--- SudokuSolver/sudokuSolver.js
+++ SudokuSolver/sudokuSolver.js
@@ -1,39 +1,105 @@
-var testarray = [[4,3,5,2,6,1,3,0], [6,7,2,1,3,null],[9,0,1,4,9,0,0], [3,2,0,2,1,0,0], [5,6,4,5,0,3], [0,9,1,0,0,1] ];
 
-
-//var numbersInGroup = {1:false, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8:false, 9:false}
 var submit = document.getElementById('submit');
 
-
-sudokuGrid = [];
+var sudokuGrid = [];
+var emptyCellsGrid = [];
+//Initialize various groups
+var group = [];
+var row = [];
+var col = [];
+var arrayInput;
 
 function getInput() {
-  var arrayInput = document.getElementsByTagName('input');
-  for (var i = 0; i < arrayInput.length; i++){
-    var cell = document.getElementsByTagName('input')[i];
-    var coord = cell.name.split(',').map(function(a){return parseInt(a)});
+  arrayInput = document.getElementsByTagName('input');
+  //Just was too lazy
+  var numberArray = [];
+  for (var j = 0; j < 9; j++) {
+    numberArray[j] = j+1;
+  }
+
+  for (var i = 0; i < arrayInput.length; i++) {
+    var cell = arrayInput[i];
+    var coord = cell.name.split(',').map( function(a){return parseInt(a)} );
 
     var cellInfoMap = {
-      'value': parseInt(cell.value),
+      'val': parseInt(cell.value),
       'coord': coord,
-      'group': groupPlacemant(coord)
+      'group': groupPlacemant(coord),
     };
-
+    if (!cell.value) {
+      cellInfoMap['possibles'] = numberArray;
+      emptyCellsGrid.push(i);
+    }
     sudokuGrid[i] = cellInfoMap;
+
+    //--Put in rightful place--
+    var groupNumber = cellInfoMap.group;
+    var rowNumber = cellInfoMap['coord'][0];
+    var colNumber = cellInfoMap['coord'][1];
+    var valIdx = cellInfoMap.val;
+    //If group-row-col idx does not exist, then init
+    if (!group[groupNumber])
+      group[groupNumber] = Array(9);
+    if (!row[rowNumber])
+      row[rowNumber] = Array(9);
+    if (!col[colNumber])
+      col[colNumber] = Array(9);
+    //Check to see if value actually exist, than assign true to val-idx in groups-rows-cols
+    if (!!valIdx) {
+      group[groupNumber][valIdx-1] = true;
+      row[rowNumber][valIdx-1] = true;
+      col[colNumber][valIdx-1] = true;
+    }
   }
 }
 
 function groupPlacemant(array) {
   var row = array[0];
   var col = array[1];
-  row %= 3;
-  col %= 3;
+  row = Math.floor(row/3)+1;
+  col = Math.floor(col/3);
 
-  var groupNumber = 3*row+(++col);
+  var groupNumber = col*3+row;
 
   return groupNumber;
 }
 
-submit.addEventListener('click', getInput);
+function solve(array) {
+  for (var m = 0; m < 2; m++) {
+    for (var i = 0; i < array.length; i++) {
+      var idx = array[i];
+      var possibles = sudokuGrid[idx].possibles;
+      var coord = sudokuGrid[idx].coord;
+      var groupNum = sudokuGrid[idx].group;
+      console.log(
+        '\nRound: '+m+
+        '\n i:'+i+
+        '\n idx: '+idx+
+        '\n coord: '+coord+
+        '\n possibles: '+possibles+
+        '\n groupNum: '+groupNum
+      );
+      for (var j = 0; j < possibles.length; j++ ) {
+        var possible = possibles[j];
+        if (group[groupNum][possible-1] || row[coord[0]][possible-1] || col[coord[1]][possible-1]) {
+          //Always wanted to do the ++/-- afterwards effect
 
-console.log(sudokuGrid);
+          console.log('\nPossible Removed: ',possibles.splice(j--, 1));
+          console.log(possibles);
+          if (possibles.length === 1) {
+            sudokuGrid[idx].value = possibles[0];
+            group[groupNum][possibles[0]-1] = true;
+            row[coord[0]][possibles[0]-1] = true;
+            col[coord[1]][possibles[0]-1] = true;
+            array.splice(i--,1);
+            arrayInput[idx].value = possibles[0];
+          }
+        }
+        sudokuGrid[idx].possibles = possibles;
+      }
+    }
+  }
+}
+
+
+submit.addEventListener('click', getInput);
