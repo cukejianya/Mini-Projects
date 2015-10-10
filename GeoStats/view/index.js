062c405dfb93318b8c135e267ec68cd4aa7d00f4
Chinedum Mac <cukejianya@gmail.com>
2015-10-10 16:47:07 -0400

    Get Autocomplete to work

--- GeoStats/view/index.js
+++ GeoStats/view/index.js
@@ -1,7 +1,16 @@
 var map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 12
   });
+  var input = document.getElementById('searchTextField');
+  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  var options = {
+    componentRestrictions: {country: 'us'}
+  };
+
+  autocomplete = new google.maps.places.Autocomplete(input, options);
+
 }
