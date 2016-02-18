7ccc17af054e56b523f5554eccced3553254b3b8
Chinedum <cukejianya@gmail.com>
2016-02-18 02:47:59 -0500

    Add broswerify and ulgify to gulp build. Also add multiple packages to project

--- GeoStats/gulpfile.js
+++ GeoStats/gulpfile.js
@@ -1,8 +1,26 @@
+var browserify = require('browserify');
 var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var bufferV = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
 
-gulp.task('default', function() {
-  gulp.src('view/js/*.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('minjs'));
+
+gulp.task('default', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './view/js/main.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('./index.js'))
+    .pipe(bufferV())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./view/js/'));
 });
