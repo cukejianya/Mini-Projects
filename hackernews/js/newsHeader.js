1c3e363699655e57ac9c55b140bcf75e7f5c9ab4
Chinedum <cukejianya@gmail.com>
2016-03-07 08:40:42 -0500

    Finish new header nav

--- hackernews/js/newsHeader.js
+++ hackernews/js/newsHeader.js
@@ -1,5 +1,6 @@
-var $ = requie('jquery');
+var $ = require('jquery');
 var React = require('react');
+var _ = require('lodash');
 
 var NewsHeader = React.createClass({
   getLogo: function() {
@@ -17,11 +18,53 @@ var NewsHeader = React.createClass({
     );
   },
 
+  getNav: () => {
+    var navLinks = [
+      {
+        name: 'new',
+        url: 'newest'
+      },
+      {
+        name: 'comments',
+        url: 'newcomments'
+      },
+      {
+        name: 'show',
+        url: 'show'
+      },
+      {
+        name: 'ask',
+        url: 'ask'
+      },
+      {
+        name: 'jobs',
+        url: 'jobs'
+      },
+      {
+        name: 'submit',
+        url: 'submit'
+      }
+    ];
+
+    return (
+      <div className="newsHeader-nav">
+        {_(navLinks).map(function (navLink) {
+          return (
+            <a key={navLink.url} className="newsHeader-navLink newsHeader-textLink" href={'https://news.ycombinator.com/' + navLink.url}>
+              {navLink.name}
+            </a>
+          );
+        }).value()}
+      </div>
+    );
+  },
+
   render: function() {
     return (
       <div className="newsHeader">
-        {this.getLog()}
+        {this.getLogo()}
         {this.getTitle()}
+        {this.getNav()}
       </div>
     );
   }
