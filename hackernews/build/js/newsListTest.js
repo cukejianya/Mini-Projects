4ce90ecf7fbc99bac73ee61f58ea25a83e1b7e57
Chinedum <cukejianya@gmail.com>
2016-03-09 08:29:39 -0500

    Add a get more articles function

--- hackernews/build/js/newsListTest.js
+++ hackernews/build/js/newsListTest.js
@@ -1723,6 +1723,14 @@ var NewsItem = require('./newsItem');
 var React = require('react');
 
 var NewsList = React.createClass({displayName: "NewsList",
+  getMore: function() {
+    return (
+      React.createElement("div", {className: "newsList-more"}, 
+        React.createElement("a", {className: "newsList-moreLink", href: "https://news.ycombinator.com/news?p=2"}, "More")
+      )
+    );
+  },
+
   render: function() {
     return (
       React.createElement("div", {className: "newsList"}, 
@@ -1731,7 +1739,8 @@ var NewsList = React.createClass({displayName: "NewsList",
             _(this.props.items).map(function (item, index) {
               return React.createElement(NewsItem, {key: item.id, item: item, rank: index + 1});
             }.bind(this)).value()
-          )
+          ), 
+          this.getMore()
         )
     );
   }
