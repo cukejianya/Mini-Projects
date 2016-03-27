fee5679c6082b2d577e3374af6f0dc1f7167522f
Chinedum <cukejianya@gmail.com>
2016-03-26 23:07:06 -0400

    Finish refracted solve function

--- SudokuSolver/sudokuSolver.js
+++ SudokuSolver/sudokuSolver.js
@@ -6,6 +6,7 @@ var row = [[],[],[],[],[],[],[],[],[]];
 var col = [[],[],[],[],[],[],[],[],[]];
 //Initialize various groups
 var group = [[],[],[],[],[],[],[],[],[]];
+var banned = [];
 var arrayInput;
 
 
@@ -44,8 +45,8 @@ function getInput() {
         'col': coord[1],
         'group': groupPlacemant(coord),
       };
-      potentialCells.push(cellInfoMap);
 
+      potentialCells.push(cellInfoMap);
     } else {
       row[coord[0]].push(cell.value);
       cell[coord[1]].push(cell.value);
@@ -54,27 +55,38 @@ function getInput() {
 
   });
 
-  solve(emptyCellsGrid);
+  solve(potentialCells);
+  
 }
 
 
-function solve(potentialCells) {
-  for (var i = 0; i < potentialCells.length; i++) {
-    cellObj = potentialCells[i];
+function solve(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    cellObj = arr[i];
     var cellRow = cellObj.row;
     var cellCol = cellObj.col;
     var cellGroup = cellObj.group;
+    var rowBool, colBool, groupBool, bannedBool;
+    if (!banned[i]) banned.push([]);
 
     for (var k = 1; k <= 9; k++) {
-      var rowBool = (-1 !== row[cellRow].indexOf(k));
-      var colBool = (-1 !== col[cellCol].indexOf(k));
-      var groupBool = (-1 !== group[cellGroup].indexOf(k));
+      rowBool = (-1 === row[cellRow].indexOf(k));
+      colBool = (-1 === col[cellCol].indexOf(k));
+      groupBool = (-1 === group[cellGroup].indexOf(k));
+      bannedBool = (-1 === banned[i].indexOf(k));
 
-      if (rowBool && colBool && groupBool) {
-        
+      if (rowBool && colBool && groupBool && bannedBool) {
+        cellObj.val = k;
+        row[cellRow].push(k);
+        col[cellCol].push(k);
+        group[cellGroup].push(k);
+        banned[i].push(k);
+        break;
       }
     }
+    if (rowBool && colBool && groupBool && bannedBool) continue;
 
+    i -= 2;
   }
 
     //   console.log(
