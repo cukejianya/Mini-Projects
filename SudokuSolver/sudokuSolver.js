8c2db7cf56e4be17cf9e5712c9d5f6d8a54bcef0
Chinedum <cukejianya@gmail.com>
2016-03-28 23:51:04 -0400

    Start refractoring code to recursion

--- SudokuSolver/sudokuSolver.js
+++ SudokuSolver/sudokuSolver.js
@@ -6,7 +6,6 @@ var row;
 var col;
 //Initialize various groups
 var group;
-var banned;
 var arrayInput;
 
 
@@ -28,7 +27,6 @@ function init() {
   col = [[],[],[],[],[],[],[],[],[]];
   //Initialize various groups
   group = [[],[],[],[],[],[],[],[],[]];
-  banned = [];
 }
 
 
@@ -84,34 +82,47 @@ function getInput() {
 
 function solve(arr) {
   for (var i = 0; i < arr.length; i++) {
-    cellObj = arr[i];
+
+    if (rowBool && colBool && groupBool && bannedBool) continue;
+
+    var prevCellObj = arr[i - 1];
+    row[prevCellObj.row].pop();
+    col[prevCellObj.col].pop();
+    group[prevCellObj.group].pop();
+    i -= 2;
+
+  }
+
+  function findCell(idx) {
+    var cellObj = arr[idx];
     var cellRow = cellObj.row;
     var cellCol = cellObj.col;
     var cellGroup = cellObj.group;
-    var rowBool, colBool, groupBool, bannedBool;
-    if (!banned[i]) banned.push([]);
+    var rowBool, colBool, groupBool;
 
     for (var k = 1; k <= 9; k++) {
       rowBool = (-1 === row[cellRow].indexOf(k));
       colBool = (-1 === col[cellCol].indexOf(k));
       groupBool = (-1 === group[cellGroup].indexOf(k));
-      bannedBool = (-1 === banned[i].indexOf(k));
 
-      if (rowBool && colBool && groupBool && bannedBool) {
+      if (rowBool && colBool && groupBool) {
         cellObj.val = k;
         row[cellRow].push(k);
         col[cellCol].push(k);
         group[cellGroup].push(k);
-        banned[i].push(k);
-        break;
-      }
-    }
-    if (rowBool && colBool && groupBool && bannedBool) continue;
 
-    i -= 2;
-    row.pop();
-    col.pop();
-    group.pop();
+        if (findCell(idx+1)) {
+          return true;
+        }
+
+        row[cellRow].pop();
+        col[cellCol].pop();
+        group[cellGroup].pop();
+      }
+
+    }
+
+    return false;
   }
 
     //   console.log(
@@ -123,6 +134,27 @@ function solve(arr) {
     //     '\n groupNum: '+groupNum
     //   );
 
+}
+
+//debug
+function printGrid(arr) {
+  var grid = "";
+  var cell;
+  for (var r = 0; r < 9; r++) {
+    for (var c = 0; c < 9; c++) {
+      for (var i = 0; i < arr.length; i++) {
+        cell = arr[i];
+        if (cell.row === r && cell.col === c) {
+          grid += cell.value
+        } else {
+
+        }
+      }
+      if (c+1 % 3 === 0) {
+        grid += "";
+      }
+    }
+  }
 }
 
 submit.addEventListener('click', getInput);
