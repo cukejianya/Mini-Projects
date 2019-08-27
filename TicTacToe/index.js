f26e775fc72d545f0f905a5beaf9c3e35922ca2d
Chinedum Ukejianya <cukejianya@gmail.com>
2019-08-27 15:12:38 -0400

    Refractor into objects and create a reset button

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,23 +1,50 @@
-const oPic = "https://www.drodd.com/images15/letter-o25.png";
-const xPic = "https://www.drodd.com/images15/letter-x14.jpg";
-let turn = 0;
+let game = {
+  setUp() {
+    console.log("Running");
+    let resetButton = document.querySelector(".reset")
+    this.playerX = Object.assign(Object.create(player), {
+      image: "https://www.drodd.com/images15/letter-x14.jpg",
+      cellsMarked: []
+    });
+    this.playerO = Object.assign(Object.create(player), {
+      image: "https://www.drodd.com/images15/letter-o25.png",
+      cellsMarked: []
+    });
+    this.reset();
+    resetButton.addEventListener("click", this.reset.bind(this));
+  },
 
-window.onload = () => {
-  let divList = document.querySelectorAll("div");;
-  divList.forEach((div) => {
-    if (div.className === "container") {
-      return;
-    }
-    div.addEventListener("click", clickEvent);
-  });
+  reset() {
+    this.turn = 0;
+    document.querySelectorAll("div").forEach((div) => {
+      if (div.className === "container") {
+        return;
+      }
+      if (div.hasChildNodes()) {
+        div.removeChild(div.firstChild);
+      }
+      div.addEventListener("click", clickEvent);
+    });
+    this.playerO.cellsMarked = [];
+    this.playerX.cellsMarked = [];
+  },
 }
 
-function clickEvent(event) {
+let clickEvent = (function (event) {
   let elm = event.currentTarget;
-  let picURL = turn % 2 === 0 ? oPic : xPic;
-  let imageElm = document.createElement('img')
-  imageElm.src = picURL;
-  turn++;
+  let player = this.turn % 2 === 0 ? this.playerO : this.playerX;
+  let imageElm = document.createElement('img');
+  player.markCell(elm);
+  imageElm.src = player.image;
+  this.turn++;
   elm.appendChild(imageElm);
   elm.removeEventListener("click", clickEvent);
+}).bind(game);
+
+let player = {
+  markCell(elm) {
+    this.cellsMarked.push(parseInt(elm.className)); 
+  },
 }
+
+window.onload = game.setUp.bind(game)
