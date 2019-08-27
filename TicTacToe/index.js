e25037086cd02e03b1d7ee500080f53a29a9d750
Chinedum Ukejianya <cukejianya@gmail.com>
2019-08-27 02:12:29 -0400

    Create grid and click event

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,7 +1,23 @@
 const oPic = "https://www.drodd.com/images15/letter-o25.png";
-const xPic = "https://www.drodd.com/images15/letter-x14.jpg".
+const xPic = "https://www.drodd.com/images15/letter-x14.jpg";
+let turn = 0;
 
 window.onload = () => {
-  let div = document.querySelectorAll("div");
-  console.log(div);
-};
+  let divList = document.querySelectorAll("div");;
+  divList.forEach((div) => {
+    if (div.className === "container") {
+      return;
+    }
+    div.addEventListener("click", clickEvent);
+  });
+}
+
+function clickEvent(event) {
+  let elm = event.currentTarget;
+  let picURL = turn % 2 === 0 ? oPic : xPic;
+  let imageElm = document.createElement('img')
+  imageElm.src = picURL;
+  turn++;
+  elm.appendChild(imageElm);
+  elm.removeEventListener("click", clickEvent);
+}
