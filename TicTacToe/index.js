2da96ce10a060ca7d3d76cc10f4dd90a9795515e
Chinedum Ukejianya <cukejianya@gmail.com>
2019-08-26 23:56:03 -0400

    Start up tic tac toe project

---
+++ TicTacToe/index.js
@@ -0,0 +1,7 @@
+const oPic = "https://www.drodd.com/images15/letter-o25.png";
+const xPic = "https://www.drodd.com/images15/letter-x14.jpg".
+
+window.onload = () => {
+  let div = document.querySelectorAll("div");
+  console.log(div);
+};
